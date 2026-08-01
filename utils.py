ae4a97aca8983a01f4f7fe5eb545790d6b29deb7
Suptemen <kirillk202010@gmail.com>
2026-08-01 19:38:12 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import cv2
 from PIL import Image
 import json
+import numpy as np
 
 def get_file_size(filepath):
     return os.path.getsize(filepath)
@@ -38,3 +39,29 @@ def clean_temp_files(folder):
 
 def get_filename_without_ext(filepath):
     return os.path.splitext(os.path.basename(filepath))[0]
+
+def generate_palette(frames, max_colors=10):
+    palette = []
+    try:
+        for frame in frames:
+            if len(frame.shape) == 3:
+                if frame.shape[2] == 3:
+                    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                else:
+                    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGBA)
+                pil_img = Image.fromarray(frame_rgb)
+                colors = pil_img.getcolors(maxcolors=256)
+                if colors:
+                    sorted_colors = sorted(colors, key=lambda x: x[0], reverse=True)
+                    for count, color in sorted_colors[:max_colors]:
+                        if len(color) == 3:
+                            hex_color = '#{:02x}{:02x}{:02x}'.format(color[0], color[1], color[2])
+                        else:
+                            hex_color = '#{:02x}{:02x}{:02x}'.format(color[0], color[1], color[2])
+                        if hex_color not in palette:
+                            palette.append(hex_color)
+                if len(palette) >= max_colors:
+                    break
+    except:
+        pass
+    return palette[:max_colors]
